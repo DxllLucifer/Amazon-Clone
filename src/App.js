03b9e5438bd25846f630244fc5ffa19aa4b49e4a
DxllLucifer <dubey.ashutosh946@gmail.com>
2023-10-03 15:34:18 +0530

    change in routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import {BrowserRouter as Router, Routes , Route} from 'react-router-dom'
-import Root from './components/Root';
+import Header from './components/Header';
+import Home from './components/Home';
 
 
 function App() {
   return (
     <Router>
       <div className="app">
+        <Header/>
         <Routes >
-          <Route path='/' element={<Root/>} />
+          <Route path='/' element={<Home/>} />
         </Routes >
       </div>
     </Router>
