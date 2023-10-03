34f05da85d997eec9a84885375ded4692f4df532
DxllLucifer <dubey.ashutosh946@gmail.com>
2023-10-03 15:34:31 +0530

    change route

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Checkout.css'
+import Header from './Header'
+
+function Checkout() {
+  return (
+  <>
+    <div className='CheckOut'>
+    </div>
+  </>
+  )
+}
+
+export default Checkout
